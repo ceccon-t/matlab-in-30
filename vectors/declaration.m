25e8c2fd3162e30e10e28c3eacc322729c9abedd
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-05 11:11:45 -0300

    More vector declaration

--- vectors/declaration.m
+++ vectors/declaration.m
@@ -2,3 +2,7 @@ a = [1 2 3 4];
 b = [1, 2, 3, 4];
 c = 1:8 % starts at 1, goes to 8, increments by 1
 d = 1:3:15 % starts at 1, goes to 15, increments by 3
+
+vector1 = a(2:3);
+vector2 = b(1:3);
+concatenated_vector = [vector1 vector2]
