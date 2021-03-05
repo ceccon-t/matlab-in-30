9bb52b478463fba80dd2adc03bb62cc0b6cf6f75
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-05 12:11:19 -0300

    Some special matrices and more matrix operations

--- matrix/declaration.m
+++ matrix/declaration.m
@@ -3,3 +3,12 @@ matrix2 = [1 3 7; 4 8 1; 9 5 -2]
 matrix3 = [2 5 8; 4 -2 -1]
 matrix4 = [1 2 7 3; -8 9 5 2; 9 6 1 0; 7 8 5 -1]
 
+zero_matrix = zeros(5)
+ones_matrix = ones(5)
+nonsquare_ones_matrix = ones(3,5)
+
+identity_matrix = eye(5)
+
+nonsquare_pseudoidentity_matrix = eye(2, 5)
+
+magic_matrix = magic(3)
--- matrix/operations.m
+++ matrix/operations.m
@@ -11,3 +11,6 @@ d = [6, 5, 1; 9, 0, 5; 8, 2, 5];
 op_scalar_multiplication = 5*c
 
 number_of_elements = numel(c)
+
+A = [1,2,3; 4,5,6; 6,7,8];
+matrix_sqrt = sqrtm(A)
