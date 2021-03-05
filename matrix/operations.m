35356aff88471df2964a52406cf6daceb20b580d
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-05 12:17:19 -0300

    More matrix operations

--- matrix/operations.m
+++ matrix/operations.m
@@ -14,3 +14,10 @@ number_of_elements = numel(c)
 
 A = [1,2,3; 4,5,6; 6,7,8];
 matrix_sqrt = sqrtm(A)
+
+A = [1,2,3,4; 5,6,7,8; 2,3,5,6; 1,2,7,2]
+op_diag_A = diag(A)
+
+op_triu_A = triu(A)
+op_tril_A = tril(A)
+
