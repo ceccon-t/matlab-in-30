2f1019a79e576e4364f0a9b8fab1f1179f831abd
Tiago Ceccon <ceccon.t@hotmail.com>
2021-03-09 02:36:16 -0300

    Add some random utilities

---
+++ utilities/demos.m
@@ -0,0 +1,5 @@
+% for Linux Matlab version 2012b, demos can be found in this location:
+% /usr/local/MATLAB/R2012b/toolbox/images/imdemos
+
+disp('For Matlab 2012b, images can be found on: ');
+fullfile(matlabroot, '\toolbox\images\imdata')
---
+++ utilities/info.m
@@ -0,0 +1,5 @@
+% use <imfinfo filename> to get info about an image file
+%
+% for example: suppose there is a file called 'a.jpg' in the current dir
+% typing 'imfinfo a.jpg', without the single quotes, would give info
+% about that file
